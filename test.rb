4336fad34df713679f8e68266da12de3b415e6d0
root <GIT_EMAIL>
2023-03-10 02:18:48 +0530

    [event_manager] tutorial

--- test.rb
+++ test.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
-p = [1, 2, 3, 4, 5]
+require 'date'
 
-p.length.times { |i| p i }
+def parseDate(date)
+  captures = date.match(/^(\d+)\/(\d+)\/(\d+)\s(\d+):(\d+)/).captures
+  time_subs = [captures[1].to_i, captures[2].to_i, captures[4].to_i, captures[5].to_i]
+  Date.new(2000 + captures[3].to_i, *time_subs)
+end
+
+p parseDate("11/12/08 10:47")
